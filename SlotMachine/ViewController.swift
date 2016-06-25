82fefa02861c468c1e3e93c5766bc465ec1c38ad
Lynx <Lynx@Lynxs-MacBook-Air.local>
2016-06-25 09:08:44 -0400

    first commit

---
+++ SlotMachine/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  SlotMachine
+//
+//  Created by Lynx on 6/24/16.
+//  Copyright © 2016 Lynx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var firstContainer : UIView!
+    var secondContainer : UIView!
+    var thirdContainer : UIView!
+    var fourthContainer : UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
