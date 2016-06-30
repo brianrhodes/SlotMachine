2e536371f0ce8aca9a2c035aac58090802f04e25
Lynx <Lynx@Lynxs-Air.fios-router.home>
2016-06-29 23:40:27 -0400

    Final Project

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Lynx on 6/27/16.
+//  Copyright © 2016 Lynx. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "")
+    var isRed = true
+}
